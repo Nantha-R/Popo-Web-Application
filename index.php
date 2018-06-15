b4dc2a91b66ff98fc65beb1f45fa0a5941d12ddb
Nantha-R <nknanthakumarcr7@gmail.com>
2018-06-15 14:38:37 +0530

    Created a base template inheritance html page

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Homepage</title>
+		<link rel="stylesheet" type="text/css" href="cssFiles/index.css">
+	</head>
+	<body>
+		<?php
+			include "base.html";
+		?>
+		<script>
+			document.getElementById('index-header').classList.add('active');
+			document.getElementById('index-header').childNodes[0].removeAttribute('href');
+		</script>
+		<div class="container">
+			<CENTER><span id="features">POPO provides you the following features</span></CENTER>
+			<div class ="row">
+				<div class="service-cards col-md-4 col-sm-4 col-lg-4">
+					<div class="card time zero-padding">
+						<div class="overlay">
+							<span>Select Time Of Delivery</span>
+						</div>
+					</div>
+				</div>
+				<div class="service-cards col-md-4 col-sm-4 col-lg-4">
+					<div class="card gps zero-padding">
+						<div class="overlay">
+							<span>Real Time Gps Monitoring</span>
+						</div>
+					</div>
+				</div>
+				<div class="service-cards col-md-4 col-sm-4 col-lg-4">
+					<div class="card bar zero-padding">
+						<div class="overlay">
+							<span>Uniquely Identify Packages Through Barcode</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
