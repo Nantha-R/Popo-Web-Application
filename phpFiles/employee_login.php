c215142a57f87b6025d361ee126e64f9a0c533c9
Nantha-R <nknanthakumarcr7@gmail.com>
2018-07-30 17:09:43 +0530

    Added backend implementation for login feature for employees.

---
+++ phpFiles/employee_login.php
@@ -0,0 +1,29 @@
+<?php
+  include 'utilities.php';
+  include 'db_utilities.php';
+  if(isset($_POST['employee_id'],$_POST['employee_password']))
+  {
+    $employee_id = $_POST['employee_id'];
+    $employee_password = $_POST['employee_password'];
+    $employee_table = 'employee';
+    $query = sprintf("SELECT * FROM %s WHERE employee_id = '%s' AND employee_password = '%s'",
+                      $employee_table,$employee_id,$employee_password);
+    $result = execute_query($query);
+    if($result->num_rows == 1)
+    {
+      store_session('employee_id',$employee_id);
+      header('Location: ../employee_portal.php');
+    }
+    else
+    {
+      store_session('employee_credentials_not_found',True);
+      header('Location: ../login.php');
+    }
+  }
+  else
+  {
+    $message = 'Invalid post request';
+    store_session("error_message",$message);
+    header('Location: ../error.php');
+  }
+?>
